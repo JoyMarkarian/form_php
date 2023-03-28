a92064076dd1cd9282e64109108f4fb11335be57
Joy Markarian <jmarkarian@hotmail.fr>
2023-03-28 17:05:40 +0200

    Form_PHP_Récupération

--- form.php
+++ form.php
@@ -5,18 +5,60 @@
 </head>
 <body>
 	<h1>Contactez-nous</h1>
+	<?php
+		$errors = [];
+		$name = "";
+		$email = "";
+		$phone = "";
+		$subject = "";
+		$message = "";
+
+		if ($_SERVER["REQUEST_METHOD"] == "POST") {
+			if (empty($_POST["user_name"])) {
+				$errors["name"] = "Le nom est obligatoire.";
+			} else {
+				$name = test_input($_POST["user_name"]);
+			}
+			if (empty($_POST["user_email"])) {
+				$errors["email"] = "L'e-mail est obligatoire.";
+			} else {
+				$email = test_input($_POST["user_email"]);
+				// Vérification que l'e-mail est au bon format
+				if (!filter_var($email, FILTER_VALIDATE_EMAIL)) {
+					$errors["email"] = "Le format de l'e-mail est invalide.";
+				}
+			}
+			if (empty($_POST["user_phone"])) {
+				$errors["phone"] = "Le téléphone est obligatoire.";
+			} else {
+				$phone = test_input($_POST["user_phone"]);
+			}
+			
+			if (empty($_POST["user_message"])) {
+				$errors["message"] = "Le message est obligatoire.";
+			} else {
+				$message = test_input($_POST["user_message"]);
+			}
+
+			if (empty($errors)) {
+				header('Location: thanks.php');
+				exit;
+			}
+		}
+
+	?>
 	<form action="thanks.php" method="post">
 		<div>
 			<label for="nom">Nom :</label>
-			<input type="text" id="nom" name="user_name">
+			<input type="text" id="nom" name="user_name" required>
 		</div>
 		<div>
 			<label for="courriel">Courriel :</label>
-			<input type="email" id="courriel" name="user_email">
+			<input type="email" id="courriel" name="user_email" required>
 		</div>
 		<div>
 			<label for="telephone">Téléphone :</label>
-			<input type="tel" id="telephone" name="user_phone">
+			<input type="tel" id="telephone" name="user_phone" required>
 		</div>
 		<div>
 			<label for="sujet">Sujet :</label>
@@ -28,7 +70,7 @@
 		</div>
 		<div>
 			<label for="message">Message :</label>
-			<textarea id="message" name="user_message"></textarea>
+			<textarea id="message" name="user_message" required></textarea>
 		</div>
 		<div class="button">
 			<button type="submit">Envoyer votre message</button>
--- thanks.php
+++ thanks.php
@@ -4,26 +4,15 @@
 	<title>Formulaire de contact</title>
 </head>
 <body>
-	<?php
-		$user_name = $_POST['user_name'];
-		$user_email = $_POST['user_email'];
-		$user_phone = $_POST['user_phone'];
-		$user_subject = $_POST['user_subject'];
-		$user_message = $_POST['user_message'];
-
-		$to = 'votre@email.com';
-		$subject = 'Nouveau message de ' . $user_name . ' - ' . $user_subject;
-		$message = 'Nom : ' . $user_name . "\r\n";
-		$message .= 'Courriel : ' . $user_email . "\r\n";
-		$message .= 'Téléphone : ' . $user_phone . "\r\n";
-		$message .= 'Sujet : ' . $user_subject . "\r\n";
-		$message .= 'Message : ' . $user_message . "\r\n";
-
-		mail($to, $subject, $message);
-
-		echo 'Merci ' . $user_name . ' de nous avoir contacté à propos de "' . $user_subject . '".<br>';
-		echo 'Un de nos conseillers vous contactera soit à l’adresse ' . $user_email . ' ou par téléphone au ' . $user_phone . ' dans les plus brefs délais pour traiter votre demande : <br>';
-		echo $user_message;
-	?>
+<?php
+if (isset($error_message)) {
+    echo '<p class="error">' . $error_message . '</p>';
+} else {
+    // Affichage du message de remerciement
+    echo 'Merci ' . $_POST['user_name'] . ' de nous avoir contacté à propos de “' . $_POST['user_subject'] . '”.<br>';
+    echo 'Un de nos conseillers vous contactera soit à l’adresse ' . $_POST['user_email'] . ' ou par téléphone au ' . $_POST['user_phone'] . ' dans les plus brefs délais pour traiter votre demande :<br>';
+    echo $_POST['user_message'];
+}
+?>
 </body>
 </html>
